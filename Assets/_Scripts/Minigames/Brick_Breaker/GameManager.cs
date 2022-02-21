f24d58b075356b011a1a6eba0a5f9554a450b006
abmoger <57551169+abmoger@users.noreply.github.com>
2022-02-21 15:06:23 -0500

    Brick Breaker Is Mostly Finished
    Still need UI, Fungus integration, and scoring.

--- Assets/_Scripts/Minigames/Brick_Breaker/GameManager.cs
+++ Assets/_Scripts/Minigames/Brick_Breaker/GameManager.cs
@@ -4,7 +4,49 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Ball ball { get; private set; }
+    public Paddle paddle { get; private set; }
+    
     public int score = 0;
 
     public int lives = 3;
+
+
+    private void Awake()
+    {
+        this.ball = FindObjectOfType<Ball>();
+        this.paddle = FindObjectOfType<Paddle>();
+    }
+
+
+    private void ResetLevel()
+    {
+        this.ball.ResetBall();
+        this.paddle.ResetPaddle();
+    }
+
+    private void GameOver()
+    {
+        Paddle.Dead();
+    }
+    
+
+    public void Miss()
+    {
+        this.lives--;
+
+        if (this.lives > 0)
+        {
+            ResetLevel();
+        } else
+        {
+            GameOver();
+        }
+    }
+    
+
+    public void Hit(Brick brick)
+    {
+        this.score += brick.points;
+    }
 }
